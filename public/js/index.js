3a4a8e46862b69195660eb89513fbc76be41abc2
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2025-10-21 14:49:29 -0700

    Thing

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,29 @@
 let firstName = document.getElementById('fname-input');
 let lastName = document.getElementById('lname-input');
 
-let form = document.getElementById('form');
+let form = document.querySelector('form');
+
+form.addEventListener('/submit-order', function(event) {
+    let valid = true;
+    if (firstName.value === ''){
+        document.getElementById('first-name-error').style.opacity = '100%';
+        valid = false;
+    }
+
+    if (lastName.value === ''){
+        document.getElementById('last-name-error').style.opacity = '100%';
+        valid = false;
+    }
+
+    if (valid === true) {
+        this.submit();
+        document.getElementById('first-name-error').style.opacity = '0%';
+        document.getElementById('last-name-error').style.opacity = '0%';
+
+    }
+    event.preventDefault();
+    
+})
 
 form.onsubmit = () => {
     if (firstName.value === ''){
