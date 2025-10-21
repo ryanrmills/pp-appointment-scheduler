6574480e4b8b7bcef3e1d5cfde96b9c34b1d97d0
Matthew Vargas <145516753+matterwoven@users.noreply.github.com>
2025-10-21 15:53:44 -0700

    Finished validation code

--- public/js/index.js
+++ public/js/index.js
@@ -3,34 +3,24 @@ let lastName = document.getElementById('lname-input');
 
 let form = document.querySelector('form');
 
-form.addEventListener('/submit-order', function(event) {
+form.addEventListener('submit', function(event) {
     let valid = true;
-    if (firstName.value === ''){
+
+
+    if (firstName.value.trim() === '') {
         document.getElementById('first-name-error').style.opacity = '100%';
         valid = false;
     }
 
-    if (lastName.value === ''){
+    if (lastName.value.trim() === '') {
         document.getElementById('last-name-error').style.opacity = '100%';
         valid = false;
     }
 
     if (valid === true) {
-        this.submit();
+        form.submit(); 
         document.getElementById('first-name-error').style.opacity = '0%';
         document.getElementById('last-name-error').style.opacity = '0%';
-
     }
     event.preventDefault();
-    
-})
-
-form.onsubmit = () => {
-    if (firstName.value === ''){
-        document.getElementById('first-name-error').style.opacity = '100%';
-    }
-
-    if (lastName.value === ''){
-        document.getElementById('last-name-error').style.opacity = '100%';
-    }
-}
+});
