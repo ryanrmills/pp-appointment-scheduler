188da2ce005d8987212bbc4d2fe56e9721c56da7
Ryan <ryanroymills@gmail.com>
2025-12-06 21:25:31 +0000

    initial commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 // Define the port number where our server will listen
-const PORT = 3011;
+const PORT = 3007;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
